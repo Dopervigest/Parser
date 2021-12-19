cc52cdca03c4507477f59b16fd0ca81b9190fd00
River002 <p.z199@yandex.ru>
2021-12-19 17:00:28 +0500

    Update Out.py

--- pythonProject/Out.py
+++ pythonProject/Out.py
@@ -1,5 +1,13 @@
 from book import Book
 
+# def out():
+#     for book in Book.list_of_books:
+#         print(book.title, book.author, book.price, book.link, sep="\n", end="\n \n")
+
+
 def out():
+    BookList = []
     for book in Book.list_of_books:
-        print(book.title, book.author, book.price, book.link, sep="\n", end="\n \n")
+        d = {'title': book.title, 'author': book.author, 'price': book.price, 'link': book.link}
+        BookList.append(d)
+    print(BookList)
