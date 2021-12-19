cc90eeabaa4f42aeed8e962a11fc13ac3b5a6788
River002 <p.z199@yandex.ru>
2021-12-19 23:37:11 +0500

    Front

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,4 +1,19 @@
 import Parser
-BookList = Parser.universal('Гарри Поттер')
+from flask import Flask, render_template
 
-print(BookList)
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/search/<variable>')
+def search(variable):
+    BookList = Parser.universal(variable)
+    return render_template("search.html", user_string=variable, list=BookList)
+
+
+if __name__ == "__main__":
+    app.run(debug="True")
